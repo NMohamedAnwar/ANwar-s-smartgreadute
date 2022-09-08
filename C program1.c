26b74cc6837241c8fd820f1b0beead38ec359e14
NMohamedAnwar <113046284+NMohamedAnwar@users.noreply.github.com>
2022-09-08 20:48:54 +0530

    Add files via upload

---
+++ C program1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+printf("hello and welcome to golden touch smart graduates program of new college...");
+printf("welcome to my program...");
+int age=22;
+char name[20]="Anwar";
+char college[20]="The New College";
+printf("\nI studied in...%s",college);
+printf("\nMy name is...%s",name);
+printf("\tMy age is...%d",age);
+
+}
