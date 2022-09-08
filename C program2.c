217d939ff7f2e38ea134da38bc9199d71d55804c
NMohamedAnwar <113046284+NMohamedAnwar@users.noreply.github.com>
2022-09-08 21:03:06 +0530

    Add files via upload

---
+++ C program2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+printf("Welcome to C programs...");
+int age="22";
+char name[20]="Anwar";
+printf("\nMy name is...%s",name);
+char c='a';
+char cc=100;
+const double pi=3.14;
+//pi=50;
+printf("\tMy pi value is....%lf",pi);
+printf("\n the size of the pi variable is...%d",sizeof(pi));
+}
